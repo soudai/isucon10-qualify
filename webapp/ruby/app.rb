6c6913e5c0ab35d6871942674fcc7ac2e2249070
Ryuta Kamizono <kamipo@gmail.com>
2020-09-12 17:43:22 +0900

    puts "/api/estate/search: ..."

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -495,6 +495,8 @@ class App < Sinatra::Base
     count = db.xquery("#{count_prefix}#{search_condition}", query_params).first[:count]
     estates = db.xquery("#{sqlprefix}#{search_condition}#{limit_offset}", query_params).to_a
 
+    puts "/api/estate/search: #{page} page, #{search_condition}, #{query_params.inspect}"
+
     { count: count, estates: estates.map! { |e| camelize_keys_for_estate(e) } }.to_json
   end
 

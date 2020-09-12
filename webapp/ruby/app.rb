66602073900c11147f6f90779db3feeefc5a70e6
Ryuta Kamizono <kamipo@gmail.com>
2020-09-12 17:34:23 +0900

    comment out

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -529,7 +529,7 @@ class App < Sinatra::Base
     sql = 'SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? ORDER BY popularity DESC, id ASC'
     estates = db.xquery(sql, bounding_box[:bottom_right][:latitude], bounding_box[:top_left][:latitude], bounding_box[:bottom_right][:longitude], bounding_box[:top_left][:longitude]).to_a
 
-    puts "nazotte: #{estates.size} estates"
+    #puts "nazotte: #{estates.size} estates"
 
     estates_in_polygon = []
     estates.each do |estate|

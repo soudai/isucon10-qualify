8bed5e48b2103a185cbc8a59d936f5a5da7b33f2
Ryuta Kamizono <kamipo@gmail.com>
2020-09-12 18:46:46 +0900

    update types

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -344,6 +344,15 @@ class App < Sinatra::Base
       CSV.parse(params[:chairs][:tempfile].read, skip_blanks: true) do |row|
         sql = 'INSERT INTO chair(id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
         db.xquery(sql, *row.map(&:to_s))
+        db.xquery(<<-SQL, row[0])
+UPDATE chair
+SET price_t = CASE WHEN price < 3000 THEN 0 WHEN price < 6000 THEN 1 WHEN price < 9000 THEN 2
+                   WHEN price < 12000 THEN 3 WHEN price < 15000 THEN 4 ELSE 5 END,
+    height_t = CASE WHEN height < 80 THEN 0 WHEN height < 110 THEN 1 WHEN height < 150 THEN 2 ELSE 3 END,
+    width_t = CASE WHEN width < 80 THEN 0 WHEN width < 110 THEN 1 WHEN width < 150 THEN 2 ELSE 3 END,
+    depth_t = CASE WHEN depth < 80 THEN 0 WHEN depth < 110 THEN 1 WHEN depth < 150 THEN 2 ELSE 3 END
+WHERE id = ?
+SQL
         if !row[9].nil? && row[9] != ''
           row[9].split(',').each do |feature|
             sql = 'INSERT INTO chair_features (name, chair_id) values (?, ?)'
@@ -585,6 +594,13 @@ class App < Sinatra::Base
       CSV.parse(params[:estates][:tempfile].read, skip_blanks: true) do |row|
         sql = 'INSERT INTO estate(id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
         db.xquery(sql, *row.map(&:to_s))
+        db.xquery(<<-SQL, row[0])
+UPDATE estate
+SET rent_t = CASE WHEN rent < 50000 THEN 0 WHEN rent < 100000 THEN 1 WHEN rent < 150000 THEN 2 ELSE 3 END,
+    door_height_t = CASE WHEN door_height < 80 THEN 0 WHEN door_height < 110 THEN 1 WHEN door_height < 150 THEN 2 ELSE 3 END,
+    door_width_t = CASE WHEN door_width < 80 THEN 0 WHEN door_width < 110 THEN 1 WHEN door_width < 150 THEN 2 ELSE 3 END 
+WHERE id = ?
+SQL
         if !row[10].nil? && row[10] != ''
           row[10].split(',').each do |feature|
             sql = 'INSERT INTO estate_features (name, estate_id) values (?, ?)'

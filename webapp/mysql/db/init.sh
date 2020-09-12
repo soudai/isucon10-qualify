6f6587e9dd43d710ca52bf04df9d25c2b1af6bc6
Ryuta Kamizono <kamipo@gmail.com>
2020-09-12 06:14:56 +0000

    mysqlを別サーバにして8にした

--- webapp/mysql/db/init.sh
+++ webapp/mysql/db/init.sh
@@ -3,7 +3,7 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+export MYSQL_HOST=${MYSQL_HOST:-10.162.86.102}
 export MYSQL_PORT=${MYSQL_PORT:-3306}
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isuumo}
